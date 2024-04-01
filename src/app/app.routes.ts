b82813e83d35fd11a3d9887c1622b3b2b767db5a
Jonathan12V <2023_jonathan.valle@iticbcn.cat>
2024-04-02 00:44:30 +0200

    Trello

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './component/inicio/inicio.component';
+import { ModuleWithProviders } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: InicioComponent}
+];
+
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
